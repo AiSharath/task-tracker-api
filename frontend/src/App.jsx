1900245bb3106871d79a4fc10a3e5a933faf7ce2
AiSharath <sharathbhat755@gmail.com>
2026-03-30 14:09:15 +0530

    Updated the server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Home from "./Home.jsx"
 import Register from "./pages/Register.jsx"
 import Login from "./pages/Login.jsx"
@@ -7,20 +7,42 @@ import CreateTask from "./pages/CreateTask.jsx"
 import TaskList from "./pages/TaskList.jsx"
 import Dashboard from "./pages/Dashboard.jsx"
 
+function getCookie(name) {
+    const cookies = document.cookie.split("; ")
+    for (let cookie of cookies) {
+        const [key, value] = cookie.split("=")
+        if (key === name) return value
+    }
+    return null
+}
 
-function App(){
-    return(
+function ProtectedRoute({ children }) {
+    const token = getCookie("token")
+    return token ? children : <Navigate to="/login" />
+}
+
+function App() {
+    return (
         <>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/tasks" element={<TaskList/>}/>
-                <Route path="/tasks/create" element={<CreateTask/>}/>
-                <Route path="/dashboard" element={<Dashboard/>}/>
+                {/* public routes */}
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+
+                {/* protected routes */}
+                <Route path="/dashboard" element={
+                    <ProtectedRoute><Dashboard /></ProtectedRoute>
+                } />
+                <Route path="/tasks" element={
+                    <ProtectedRoute><TaskList /></ProtectedRoute>
+                } />
+                <Route path="/tasks/create" element={
+                    <ProtectedRoute><CreateTask /></ProtectedRoute>
+                } />
             </Routes>
         </>
     )
 }
 
-export default App;
+export default App
